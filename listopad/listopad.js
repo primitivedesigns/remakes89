360cd484050a72d413fa1d266c24e09034fed2a0
raguklemenso <52992266+raguklemenso@users.noreply.github.com>
2019-11-07 23:22:48 +0100

    Update listopad.js

--- listopad/listopad.js
+++ listopad/listopad.js
@@ -532,7 +532,7 @@ const actions = [{
     keys: ["v"],
     perform: function(game, params) {
         game.clearOutput();
-        const takeableItems = game.location.items.map(i => game.mapItem(i)).filter(item => item.takeable === undefined || item.takeable || item.name === "bednu");
+        const takeableItems = game.location.items.map(i => game.mapItem(i)).filter(item => item.takeable === undefined || item.takeable || item.name === "bednu" || item.name === "vrata" || item.name === "poklop" || item.name === "dveře");
         if (!takeableItems || takeableItems.length === 0) {
             game.print("Nic tu není!");
         } else {
